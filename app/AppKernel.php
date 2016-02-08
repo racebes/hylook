21f27e0d48c468807d18021dc6138f425ac5b91e
racebes <racebes@globales.es>
2016-02-08 13:23:19 +0100

    Revert "Entidades creadas"
    This reverts commit e51b4675ad5877b0396ac67312384a2b283dddc4.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new hylook\MainBundle\hylookMainBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- src/hylook/MainBundle/hylookMainBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace hylook\MainBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class hylookMainBundle extends Bundle
-{
-}
